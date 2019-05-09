98835a832553ef1a0b758e1ee2c69fa4627a2f99
michaeldegroot <specamps@gmail.com>
2019-05-09 16:13:30 +0200

    docs update

--- monkeyset.js
+++ monkeyset.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const fs = require('fs')
 const crypto = require('crypto')
-const zlib = require('zlib')
 
 class MonkeySet {
   /**
@@ -25,6 +24,7 @@ class MonkeySet {
   /**
     * @summary Set a filename to work with
     * @param {string} - filename
+    * @returns {class} MonkeySet class
   */
   file(filename = 'monkeyset') {
     this.filename = filename
@@ -39,13 +39,8 @@ class MonkeySet {
   */
   save (cb, savePath = this.defaultFilePath) {
     const saveFile = path.join(savePath + `/${this.filename}`)
-    fs.writeFile(`${saveFile}.json`, JSON.stringify(this.export()), err => {
-      const zip = zlib.createGzip()
-      const read = fs.createReadStream(`${saveFile}.json`)
-      const write = fs.createWriteStream(`${saveFile}.monkeyset`)
-      const stream = read.pipe(zip).pipe(write)
-      fs.unlink(`${saveFile}.json`, () => {})
-      stream.on('finish', cb.bind(this))
+    fs.writeFile(`${saveFile}.monkeyset`, JSON.stringify(this.export()), err => {
+      cb()
     })
   }
 
@@ -56,20 +51,15 @@ class MonkeySet {
   */
   load (cb, loadPath = this.defaultFilePath) {
     const loadFile = path.join(loadPath + `/${this.filename}`)
-    const fileContents = fs.createReadStream(`${loadFile}.monkeyset`)
-    const writeStream = fs.createWriteStream(`${loadFile}.json`)
-    const unzip = zlib.createGunzip()
-    const stream = fileContents.pipe(unzip).pipe(writeStream)
-    stream.on('finish', () => {
-      this.import(JSON.parse(fs.readFileSync(`${loadFile}.json`)))
-      fs.unlink(`${loadFile}.json`, cb.bind(this))
-    })
+    this.import(JSON.parse(fs.readFileSync(`${loadFile}.monkeyset`)))
+    cb()
   }
 
   /**
     * @summary Exports a MonkeySet data format
     * @param {callback} - callback
     * @param {string} - Folder to save to
+    * @returns {object} MonkeySet data format export object
   */
   export() {
     this.validate()
@@ -88,6 +78,7 @@ class MonkeySet {
   /**
     * @summary Imports a exported MonkeySet data format
     * @param {data} - MonkeySet data format to import
+    * @returns {boolean} returns true if success
   */
   import(data) {
     const contents = {
@@ -105,6 +96,7 @@ class MonkeySet {
   /**
     * @summary Grabs the last amount of sets of a MonkeySet
     * @param {number} - Amount to grab
+    * @returns {class} MonkeySet class
   */
   last(amount = 1) {
     this.chainSet = this.chainSet.slice(Math.max(this.chainSet.length - amount, 1))
@@ -114,6 +106,7 @@ class MonkeySet {
   /**
     * @summary Grabs the first amount of sets of a MonkeySet
     * @param {number} - Amount to grab
+    * @returns {class} MonkeySet class
   */
   first(amount = 1) {
     this.chainSet = this.chainSet.slice(0, amount)
@@ -124,6 +117,7 @@ class MonkeySet {
     * @summary Sorts a MonkeySet based on order and column specified
     * @param {string} - Order to sort by
     * @param {string} - Column to sort on
+    * @returns {class} MonkeySet class
   */
   sort(order = 'ascending', column = 'time') {
     // We are dealing with a array in array
@@ -159,7 +153,7 @@ class MonkeySet {
 
   /**
     * @summary Validates a MonkeySet
-    * @param {number} - Amount to grab
+    * @returns {class} MonkeySet class
   */
   validate() {
     this.chainSetOld = this.chainSet
@@ -186,6 +180,7 @@ class MonkeySet {
 
   /**
     * @summary Resets a MonkeySet object, clearing all data.
+    * @returns {class} MonkeySet class
   */
   reset() {
     this.time = []
@@ -207,6 +202,10 @@ class MonkeySet {
     return this.chainSet
   }
 
+  /**
+    * @summary Starts the initial data feed for the chain methods, this gets sets as a whole
+    * @returns {class} MonkeySet class
+  */
   rows() {
     this.chainSet = []
     for (let i = 0; i < this.time.length; i++) {
@@ -216,12 +215,22 @@ class MonkeySet {
     return this
   }
 
+
+  /**
+    * @summary Starts the initial data feed for the chain methods, this gets sets as a whole
+    * @param {string} - Column name to grab
+    * @returns {class} MonkeySet class
+  */
   row(column) {
     this.chainSet = this[column]
 
     return this
   }
 
+  /**
+    * @summary Starts the initial data feed for the chain methods, this gets sets as a whole
+    * @param {array[]} - A set or a array of sets
+  */
   add(...newSet) {
     for (let set of newSet) {
       if (set.length != 6) continue
